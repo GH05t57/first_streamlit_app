d3fc517df93d5e6e2d1b8f7f382d3429ec19aad2
Andy Thompson <118211965+GH05t57@users.noreply.github.com>
2022-11-14 15:02:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-stereamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
