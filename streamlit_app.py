d84b7ea4001d1129dfa987de63bd0bbde39948b5
Andy Thompson <118211965+GH05t57@users.noreply.github.com>
2022-11-18 16:57:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -31,16 +32,20 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    # normalize json data
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # create table from data
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# normalize json data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# create table from data
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #stop running below
 streamlit.stop()
