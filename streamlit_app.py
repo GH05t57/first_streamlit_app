17db773b38d71c9cbfbb42019844d0d178f8ea37
Andy Thompson <118211965+GH05t57@users.noreply.github.com>
2022-11-21 12:22:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,5 +75,10 @@ def insert_row_snowflake(new_fruit):
             my_cnx.close()
             return "Thanks for adding " + new_fruit
        
-    
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
