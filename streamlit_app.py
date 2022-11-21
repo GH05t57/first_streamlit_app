542f2647c151e8386065968f5a83c47295086536
Andy Thompson <118211965+GH05t57@users.noreply.github.com>
2022-11-21 11:25:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable cod block (called a function)
-def get_fruityvice_data(this_fruit_choice):
+def get_fruityvice_data(fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
@@ -70,6 +70,4 @@ def insert_row_snowflake(new_fruit):
             return "Thanks for adding " + new_fruit
        
     
-    import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
