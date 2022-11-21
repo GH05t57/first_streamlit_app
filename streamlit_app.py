d6e2374f230001206e68135c3a64ccb7ef3c4985
Andy Thompson <118211965+GH05t57@users.noreply.github.com>
2022-11-21 12:47:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,4 @@
 import streamlit
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
-
-
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -16,10 +10,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas
-#import requests
-#import snowflake.connector
-#from urllib.error import URLError
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -35,7 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable cod block (called a function)
-def get_fruityvice_data(fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
@@ -53,14 +47,13 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
+#streamlit.stop()
 streamlit.header("The fruit load list contains:")
-Snowflake-related functions
+#Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
       my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
-
 
 #add a button to load the fruit
 if streamlit.button('Get Fruit List'):
@@ -70,11 +63,9 @@ if streamlit.button('Get Fruit List'):
     streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
-#def insert_row_snowflake(new_fruit):
-#        with my_cnx.cursor() as my_cur:
-#            my_cur.execute("insert into fruit_load_list values ('"  INSERT INTO fruit_load_list(  "')")
-#            my_cnx.close()
-#            return "Thanks for adding " + new_fruit
-     
-
-
+def insert_row_snowflake(new_fruit):
+        with my_cnx.cursor() as my_cur:
+            my_cur.execute("insert into fruit_load_list values ('" + new_fruit   + "')")
+            my_cnx.close()
+            return "Thanks for adding " + new_fruit
+       
