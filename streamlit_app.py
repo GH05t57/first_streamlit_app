640577bda1ccb778e0a965d1bd35cf89496a8f42
Andy Thompson <118211965+GH05t57@users.noreply.github.com>
2022-11-21 12:14:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,10 +16,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
+#import pandas
+#import requests
+#import snowflake.connector
+#from urllib.error import URLError
 
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
